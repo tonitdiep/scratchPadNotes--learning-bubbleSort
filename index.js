942443d404cb1d11d5aabd34f6df0087a760c1cb
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-02-10 04:30:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,98 @@
+// function bubbleSort(arr){
+  //basic form/brute force
+//     for(var i =0; i<arr.length; i++){
+//       console.log(arr)
+//         for(var j = 0; j < arr.length; j++) {
+//                  console.log('inner loop', arr[j], arr[j+1])
+//             if(arr[j] > arr[j+1]){
+//               [arr[j] = arr[j+1]] = [arr[j+1] = arr[j]]
+//                 // var temp = arr[j];
+//                 // arr[j] = arr[j + 1];
+//                 // arr[j+1] = temp;
+//             }    
+  
+//         }
+//     }
+       
+//     return arr;
+
+// }
+
+// bubbleSort([37, 45, 29, 8]);
+
+// function bubbleSort(arr){
+//     // for(var i =0; i < arr.length; i--){
+//     for(var i = arr.length-1; i > 0; i--){
+//       //  console.log('first for loop', arr, arr[j], arr[j+1])
+//         for(var j = 0; j < i - 1; j++) {
+
+//           console.log('2nd for inner loop', arr, arr[j], arr[j+1])
+
+//             if(arr[j] > arr[j+1]){
+//               //  console.log('2nd loop if statement', arr, arr[j], arr[j+1])
+//                 var temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j+1] = temp;
+//             }    
+//             //  console.log('outside if loop', arr, arr[j], arr[j+1])
+//         }
+//         //  console.log('out for2nd loop', arr, arr[j], arr[j+1])
+//     }
+//     console.log('ONE PASS COMPLETE')
+// //  console.log('outside entire loop', arr, arr[j], arr[j+1])
+//     return arr;
+
+// }
+
+// bubbleSort([37, 45, 29, 8, 12, 88, -3]);
+
+
+
+
+// function bubbleSort(arr){
+//   const swap = (arr, idx1, idx2) => {
+//       [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//   };
+
+//     for(let i = arr.length; i > 0; i--){
+//         for(let j = 0; j < i - 1; j++) {
+
+         
+
+//             if(arr[j] > arr[j+1]){
+//                 // var temp = arr[j];
+//                 // arr[j] = arr[j + 1];
+//                 // arr[j+1] = temp;
+//                 swap(arr, j, j + 1);
+//             }    
+            
+//         }
+//         // console.log(arr, arr[j], arr[i], arr[j+1], arr[i-1])
+//     }
+//     return arr;
+
+// }
+
+// bubbleSort([37, 45, 29, 8, 12]);
+
+
+function bubbleSort(arr) {
+	var noSwaps;
+	for(var i = arr.length; i > 0; i--){
+		noSwaps = true;
+   
+		for(var j = 0; j < i -1; j++){
+     
+			if(arr[j] > arr[j + 1]){
+				var temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j =+ 1] = temp;
+				noSwaps = false;
+			}
+		}
+     console.log('checking', arr, arr[i], arr[j], arr[j+1])
+		if(noSwaps) break;
+	}
+	return arr;
+}
+bubbleSort([37, 45, 29, 8, 12, 88, -3]);
